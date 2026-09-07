ba4b1ee96a24b4169da03a0f5f8cd61ff5122445
nguyen05566 <javjp606@gmail.com>
2026-09-07 20:20:16 +0700

    Change CARO_USER_DIRECT to 'nguyen10'

--- zaro20.py
+++ zaro20.py
@@ -20,7 +20,7 @@ import random
 
 # ==================== TÀI KHOẢN (KHÔNG CẦN COOKIE) ====================
 # Đăng nhập trực tiếp bằng username/password giống các bot nguyen1..nguyen6
-CARO_USER_DIRECT = "**arena1"
+CARO_USER_DIRECT = "nguyen10"
 CARO_PASSWD_DIRECT = "******"
 BOT_DISPLAY_NAME = "Lobby.xiangqi.0"  # Tên hiển thị của bot trên game
 
