bcf9652b9f0eb8c77316fc1aa0af14d70dfed52c
nguyen05566 <javjp606@gmail.com>
2026-09-08 10:14:30 +0700

    Update nguyen4.py

--- nguyen4.py
+++ nguyen4.py
@@ -372,8 +372,8 @@ class EmbryoEngine:
 WS_URL = "wss://gamevh.net/ws/gameServer"
 GAME_URL = "https://gamevh.net/play/caro/0"
 
-CARO_USER_DIRECT = "nguyen6"
-CARO_PWWD_DIRECT = "abc123"
+CARO_USER_DIRECT = "nguyen12"
+CARO_PWWD_DIRECT = "******"
 
 def _clean_env(val: Optional[str], default: str) -> str:
     if val and str(val).strip(): return str(val).strip()
