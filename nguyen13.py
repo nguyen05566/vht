6733490e7ca31570625a087faeed4118719d90a0
nguyen05566 <javjp606@gmail.com>
2026-09-13 03:02:59 +0700

    Change BOT_BET_XU value from 1000 to 400

--- nguyen13.py
+++ nguyen13.py
@@ -388,7 +388,7 @@ RUNTIME = int(os.environ.get("CARO_RUNTIME_SECONDS") or
               float(os.environ.get("CARO_RUNTIME_HOURS", "5.9")) * 3600)
 AUTO_IDENTITY = os.environ.get("CARO_AUTO_IDENTITY", "1") == "1"
 IDENTITY_TEST_ONLY = os.environ.get("CARO_IDENTITY_TEST_ONLY", "0") == "1"
-BOT_BET_XU = 1000
+BOT_BET_XU = 400
 BOT_MATCH_DURATION = '1800'
 BOT_TURN_DURATION = '60'
 # Đếm ngược server khi đối phương VÀO BÀN (accDuration):
